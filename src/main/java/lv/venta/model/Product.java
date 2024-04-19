48628a2918ea533f768b53cb2b8f593cb6942d91
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-19 10:15:44 +0300

    Add: added validation in Product class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,4 +1,10 @@
 package lv.venta.model;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 //just comment
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -16,9 +22,23 @@ public class Product {
 	@Setter(value = AccessLevel.NONE)
 	private int id;
 		
+	@NotNull
+	@Size(min = 3, max = 50)
+	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅa-zēūīļķģšāžčņ ]+")
 	private String title;
+	
+	@Max(1000)
+	@Min(0)
 	private float price;
+	
+	
+	@NotNull
+	@Size(min = 4, max = 100)
+	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅa-zēūīļķģšāžčņ.!:- ]+")
 	private String description;
+	
+	@Max(500)
+	@Min(0)
 	private int quantity;
 	
 	private static int counter = 0;
