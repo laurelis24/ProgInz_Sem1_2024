4ac85b1f294c721405882e566f7aab4be3e744f5
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-19 12:01:00 +0300

    Add: added Filtering service interfacse for Product

---
+++ src/main/java/lv/venta/service/IProductFilteringService.java
@@ -0,0 +1,16 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+import lv.venta.model.Product;
+
+public interface IProductFilteringService {
+	public abstract ArrayList<Product> filterByPriceLess(float threshold);
+	
+	public abstract ArrayList<Product> filterByQuantityLess(int threshold);
+	
+	public abstract ArrayList<Product> filterByTitleOrDescription(String phrase);
+	
+	public abstract float calculateTotalValueOfProducts();
+	
+}
