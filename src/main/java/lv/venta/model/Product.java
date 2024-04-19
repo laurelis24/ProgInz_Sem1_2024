c0e320972701ae937369779cdf0c475e948a5492
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-19 10:31:28 +0300

    Add: added data JP for Product class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,4 +1,10 @@
 package lv.venta.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -18,38 +24,40 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 //@AllArgsConstructor
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)//autoincrement
 	private int id;
 		
 	@NotNull
 	@Size(min = 3, max = 50)
 	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅa-zēūīļķģšāžčņ ]+")
+	@Column(name = "Title")
 	private String title;
 	
 	@Max(1000)
 	@Min(0)
+	@Column(name = "Price")
 	private float price;
 	
 	
 	@NotNull
 	@Size(min = 4, max = 100)
 	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅa-zēūīļķģšāžčņ.!:- ]+")
+	@Column(name = "Description")
 	private String description;
 	
 	@Max(500)
 	@Min(0)
+	@Column(name = "Quantity")
 	private int quantity;
 	
-	private static int counter = 0;
-	
-	public void setId() {
-		id = counter;
-		counter++;
-	}
 	
 	public Product(String title, float price, String description, int quantity) {
-		setId();
 		setTitle(title);
 		setPrice(price);
 		setDescription(description);
