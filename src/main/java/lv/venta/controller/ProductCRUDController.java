297c8bed4300467b2b43c33f5b03ab886101c6bf
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-26 11:42:59 +0300

    Add: findbyid in controller layer

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -1,11 +1,15 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import lv.venta.model.Product;
 import lv.venta.service.IProductCRUDService;
 
 @Controller
@@ -19,12 +23,35 @@ public class ProductCRUDController {
 	public String getProductCRUDAll(Model model) 
 	{
 		try {
-			model.addAttribute("mydata", crudService.retrieveAll());
+			ArrayList<Product> allProducts = crudService.retrieveAll();
+			model.addAttribute("mydata", allProducts);
 			return "product-show-all-page";//tiks parādīta producty-show-all-page.html ar visiem produktiem
 		} catch (Exception e) {
 			model.addAttribute("mydata", e.getMessage());
 			return "error-page";
 		}
 	}
+	
+	@GetMapping("/one") //localhost:8080/product/crud/one?id=2
+	public String getProductCRUDOne(@RequestParam("id") int id, Model model)
+	{
+		try
+		{
+			Product foundProduct = crudService.retrieveById(id);
+			model.addAttribute("mydata", foundProduct);
+			return "product-show-one-page";//tiks parādīta product-show-one-page.html lapa
+		}
+		catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
