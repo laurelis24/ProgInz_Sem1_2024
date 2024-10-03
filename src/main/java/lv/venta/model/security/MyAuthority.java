0cacf1744440444191abd5d775698dde97604f82
laurelis <lauris-kairo@inbox.lv>
2024-10-03 13:26:33 +0300

    2 repos

--- src/main/java/lv/venta/model/security/MyAuthority.java
+++ src/main/java/lv/venta/model/security/MyAuthority.java
@@ -14,7 +14,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.val;
+
 
 @Setter
 @Getter
---
+++ src/main/java/lv/venta/repo/security/IMyUserRepo.java
@@ -0,0 +1,9 @@
+package lv.venta.repo.security;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.model.security.MyUser;
+
+public interface IMyUserRepo extends CrudRepository<MyUser, Integer> {
+
+}
